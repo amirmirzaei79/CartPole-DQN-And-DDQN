cf8456e9b59b8d4779b92430fa7d981cddaaea47
Amir Mirzaei <amir.mirzaei1379@gmail.com>
2019-06-04 13:42:41 +0430

    First succesful implementation - Not working properly yet!

--- main.py
+++ main.py
@@ -11,13 +11,60 @@ model = keras.models.Sequential()
 model.add(keras.layers.Dense(32, activation='sigmoid', input_dim=env.observation_space.shape[0]))
 model.add(keras.layers.Dense(32, activation='sigmoid'))
 model.add(keras.layers.Dense(env.action_space.n, activation='linear'))
-model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(0.001)) # 0.001 is learning rate of Adam
+model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(0.001), metrics=['mae']) # 0.001 is learning rate of Adam
 
-env.reset()
-env.render()
-observations, Reward, done, info = env.step(env.action_space.sample())
-env.render()
+# deep Q learning init
+gamma = 0.9
+epsilon = 1.0
+epsilonMin = 0.01
+epsilonDecay = 0.95
+episodeLimit = 5000
 
-print(observations, '\n', Reward, done, '\n', info)
+# deep Q
+for episode in range(episodeLimit):
+    currentStateArray = env.reset()
+    currentState = np.array([currentStateArray])
+    done = False
+    while not done:
+        # env.render()
 
+        if np.random.rand() <= epsilon:
+            action = np.random.randint(0, 1)
+        else:
+            action = np.argmax(model.predict(currentState))
+
+        newStateArray, reward, done, info = env.step(action)
+        newState = np.array([newStateArray])
+        target = reward + gamma * np.max(model.predict(newState))
+        targetLabel = model.predict(currentState)[0]
+        targetLabel[action] = target
+        model.fit(currentState, targetLabel.reshape(1, 2), epochs=1, verbose=0)
+        currentState = newState
+    else:
+        print(episode)
+
+    if epsilon > epsilonMin:
+        epsilon *= epsilonDecay
+
+model.save_weights("weights.h5")
+
+# Play game
+print("\nPlaying Game...")
 time.sleep(1)
+
+s = env.reset()
+done = False
+while not done:
+    env.render()
+    a = np.argmax(model.predict(np.array([s])))
+    newS, r, done, _ = env.step(a)
+    s = newS
+    time.sleep(0.01)
+# env.reset()
+# env.render()
+# observations, reward, done, info = env.step(env.action_space.sample())
+# env.render()
+#
+# print(observations, '\n', Reward, done, '\n', info)
+#
+# time.sleep(1)
